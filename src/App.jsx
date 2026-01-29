a7937a2b7a9c9bb4537a71255c81d97a3c4a52de
ssr0016 <samsonrecluta0016@gmail.com>
2026-01-29 19:17:03 +0800

    thinking about state and lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,7 +25,7 @@ export default function App() {
   function Logo() {
     return <h1>🌴 Far Away 💼</h1>;
   }
-  function Form() {
+  function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -30,6 +36,9 @@ export default function App() {
 
       const newItem = { description, quantity, packed: false, id: Date.now() };
       console.log(newItem);
+
+      onAddItems(newItem);
+
       setDescription("");
       setQuantity(1);
     }
@@ -58,11 +67,11 @@ export default function App() {
     );
   }
 
-  function PackingList() {
+  function PackingList({ items }) {
     return (
       <div className="list">
         <ul>
-          {initialItems.map((item) => (
+          {items.map((item) => (
             <Item item={item} key={item.id} />
           ))}
         </ul>
